5e0cd46bf3edb10646513bc26d1c45176109db1f
Inna Kostich <114675831+innakostich@users.noreply.github.com>
2023-03-15 23:12:15 +0100

    adds task 1

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,23 @@
+
+import '../css/common.css';
+
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let intervalId = null;
+
+startButton.addEventListener('click', () => {
+  startButton.disabled = true; 
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+stopButton.addEventListener('click', () => {
+  startButton.disabled = false; 
+  clearInterval(intervalId);
+});
