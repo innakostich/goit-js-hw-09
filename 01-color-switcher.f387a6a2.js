7ddbcde8c6553b14fe7bb876eb7f401430b9609e
innakostich <114675831+innakostich@users.noreply.github.com>
2023-03-15 22:12:38 +0000

    Deploying to gh-pages from @ innakostich/goit-js-hw-09@5e0cd46bf3edb10646513bc26d1c45176109db1f 🚀

---
+++ 01-color-switcher.f387a6a2.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");var n=null;t.addEventListener("click",(function(){t.disabled=!0,n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,clearInterval(n)}))}();
+//# sourceMappingURL=01-color-switcher.f387a6a2.js.map
