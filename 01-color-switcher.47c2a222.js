d3ca910ac266f41276d0285898fb0313c59491f5
innakostich <114675831+innakostich@users.noreply.github.com>
2023-04-04 14:21:51 +0000

    Deploying to gh-pages from @ innakostich/goit-js-hw-09@4a5757bbcb92a14847f71d436249022a6ca1aafa 🚀

---
+++ 01-color-switcher.47c2a222.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");let d=null;e.addEventListener("click",(()=>{e.disabled=!0,t.disabled=!1,d=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),t.addEventListener("click",(()=>{t.disabled=!0,e.disabled=!1,clearInterval(d)}));
+//# sourceMappingURL=01-color-switcher.47c2a222.js.map
--- 01-color-switcher.51f11869.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");let a=null;e.addEventListener("click",(()=>{e.disabled=!0,a=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),t.addEventListener("click",(()=>{e.disabled=!1,clearInterval(a)}));
-//# sourceMappingURL=01-color-switcher.51f11869.js.map
